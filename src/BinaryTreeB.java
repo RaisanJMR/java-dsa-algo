4f3272cffa90e3c435ec6e50b99f2ce3ae129021
RaisanJMR <raisanjmr@protonmail.com>
2022-08-19 07:53:27 +0530

    added tree traversals

--- src/BinaryTreeB.java
+++ src/BinaryTreeB.java
@@ -27,10 +27,44 @@ public class BinaryTreeB {
         }
     }
 
+    // Preorder Root->Left->Right O(n)
+    public static void preOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.println(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    // InOrder Left Root Right
+    public static void inOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.println(root.data + " ");
+        inOrder(root.right);
+    }
+
+    // postOrder Left Right Root
+    public static void postOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.println(root.data + " ");
+    }
+
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.println(root.data);
+        // System.out.println(root.data);
+        // preOrder(root);
+        // inOrder(root);
+
+        postOrder(root);
     }
 }
