63cf268e359a720f311a088ca304acf858d37b07
RaisanJMR <raisanjmr@protonmail.com>
2022-08-24 08:31:06 +0530

    added BST print Root to leaf

--- src/RLPath.java
+++ src/RLPath.java
@@ -1,7 +1,82 @@
+import java.util.ArrayList;
+
 // Root to Lead path in BST
 
 public class RLPath {
-    public static void main(String[] args) {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    // Insert
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+        if (root.data > val) {
+            root.left = insert(root.left, val);
+        } else {
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+
+    // Print
+    public static void inOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
+    public static void printPath(ArrayList<Integer> path) {
+        for (int i = 0; i < path.size(); i++) {
+            System.out.print(path.get(i) + "->");
+        }
+        System.out.println();
+    }
+
+    // Print Root To Leaf
+    public static void printRootToLeaf(Node root, ArrayList<Integer> path) {
+        if (root == null) {
+            return;
+        }
+
+        path.add(root.data);
+
+        if (root.left == null && root.right == null) {
+            printPath(path);
+        } else {
+
+            printRootToLeaf(root.left, path);
+            printRootToLeaf(root.right, path);
+        }
+
+        path.remove(path.size() - 1);
 
     }
+
+    public static void main(String[] args) {
+        int values[] = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
+        Node root = null;
+
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+
+        inOrder(root);
+        System.out.println();
+        
+        printRootToLeaf(root, new ArrayList<>());
+
+    }
+
 }
