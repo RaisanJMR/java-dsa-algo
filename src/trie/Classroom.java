20cf2531292ac02ddb7ff61e358e8eeffb27fde9
RaisanJMR <raisanjmr@protonmail.com>
2022-12-20 20:53:47 +0530

    word break

--- src/trie/Classroom.java
+++ src/trie/Classroom.java
@@ -19,7 +19,7 @@ public class Classroom {
     // created root var of type Node
     static Node root = new Node();
 
-    // INSERT O(L) -> length of string
+    // * INSERT O(L) -> length of string
     // a - a = 0
     // b - a = 1
     // c - a = 2
@@ -41,10 +41,11 @@ public class Classroom {
         }
     }
 
-    // SEARCH
+    // * SEARCH
     // O(L)
     public static boolean search(String key) {
         Node curr = root;
+        // System.out.println(curr);
         for (int i = 0; i < key.length(); i++) {
             int idx = key.charAt(i) - 'a';
             Node node = curr.children[idx];
@@ -61,14 +62,37 @@ public class Classroom {
         return true;
     }
 
+    // * WORD BREAK
+    public static boolean wordBreak(String key) {
+        if (key.length() == 0) {
+            return true;
+        }
+        for (int i = 1; i <= key.length(); i++) {
+            String firstPart = key.substring(0, i);
+            // System.out.println(firstPart);
+            String secondPart = key.substring(i);
+            // System.out.println(secondPart);
+            if (search(firstPart) && wordBreak(secondPart)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        String words[] = { "the", "a", "there", "their", "any" };
+        // String words[] = { "the", "a", "there", "their", "any" };
+        String words[] = { "i", "like", "sam", "samsung", "mobile", "ice" };
+        String key = "ilikesamsung";
+        // ? INSERT
         for (int i = 0; i < words.length; i++) {
             insert(words[i]);
         }
+        // ? SEARCH
+        // System.out.println(search("the"));
+        // System.out.println(search("thor"));
+        // System.out.println(search("an"));
 
-        System.out.println(search("the"));
-        System.out.println(search("thor"));
-        System.out.println(search("an"));
+        // ? WORD BREAK
+        System.out.println(wordBreak(key));
     }
 }
