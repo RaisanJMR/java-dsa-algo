99e4c01399f7e79f8c8d714f780a72b932be303e
RaisanJMR <raisanjmr@protonmail.com>
2022-12-22 10:35:28 +0530

    longest word with all prefixes using trie

--- src/trie/Classroom.java
+++ src/trie/Classroom.java
@@ -79,20 +79,95 @@ public class Classroom {
         return false;
     }
 
-    public static void main(String[] args) {
-        // String words[] = { "the", "a", "there", "their", "any" };
-        String words[] = { "i", "like", "sam", "samsung", "mobile", "ice" };
-        String key = "ilikesamsung";
-        // ? INSERT
-        for (int i = 0; i < words.length; i++) {
-            insert(words[i]);
+    // * Starts with
+    public static boolean startsWith(String prefix) {
+        Node curr = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            int idx = prefix.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
         }
+        return true;
+    }
+
+    // * Unique substr
+    public static int countNodes(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int count = 0;
+        for (int i = 0; i < 26; i++) {
+            if (root.children[i] != null) {
+                count += countNodes(root.children[i]);
+            }
+        }
+        return count + 1;
+    }
+
+    // * Longest word with all prefixes
+    public static String ans = "";
+
+    public static void longestWord(Node root, StringBuilder temp) {
+        if (root == null) {
+            return;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (root.children[i] != null && root.children[i].eow == true) {
+                // * (char)(0+'a') = a
+                // * (char)(1+'a') = b
+                // * (char)(2+'a') = c
+                temp.append((char) (i + 'a'));
+                System.out.println(temp);
+                if (temp.length() > ans.length()) {
+                    ans = temp.toString();
+                }
+                longestWord(root.children[i], temp);
+                temp.deleteCharAt(temp.length() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        // ! String words[] = { "the", "a", "there", "their", "any" };
+        // ! String words[] = { "i", "like", "sam", "samsung", "mobile", "ice" };
+
+        // String key = "ilikesamsung";
+
+        // ? INSERT
+        // for (int i = 0; i < words.length; i++) {
+        // insert(words[i]);
+        // }
+
         // ? SEARCH
         // System.out.println(search("the"));
         // System.out.println(search("thor"));
         // System.out.println(search("an"));
 
         // ? WORD BREAK
-        System.out.println(wordBreak(key));
+        // System.out.println(wordBreak(key));
+
+        // ? Starts with
+        // String prefix = "saas";
+        // System.out.println(startsWith(prefix));
+
+        // ? Unique substr
+        // String str = "ababa";
+        // for (int i = 0; i < str.length(); i++) {
+        // String suffix = str.substring(i);
+        // System.out.println(suffix);
+        // insert(suffix);
+        // }
+        // System.out.println(countNodes(root));
+
+        // ? Longest word with all prefixes
+        String words[] = { "a", "banana", "app", "appl", "ap", "apply", "apple" };
+        for (int i = 0; i < words.length; i++) {
+            insert(words[i]);
+        }
+        longestWord(root, new StringBuilder(""));
+        System.out.println(ans);
     }
+
 }
