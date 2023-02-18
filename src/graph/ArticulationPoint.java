4f1c9994636597480f4061d29a5b11089149c34a
RaisanJMR <raisanjmr@protonmail.com>
2023-02-18 10:25:22 +0530

    FEAT: added articulation point

---
+++ src/graph/ArticulationPoint.java
@@ -0,0 +1,110 @@
+
+// @ Articulation point in UD, UW graph
+// @ TC O(V+E)
+import java.util.*;
+
+public class ArticulationPoint {
+    static class Edge {
+        int src;
+        int dest;
+
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<Edge>();
+        }
+
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+        graph[0].add(new Edge(0, 3));
+
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 2));
+
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 1));
+
+        graph[3].add(new Edge(3, 0));
+        graph[3].add(new Edge(3, 4));
+
+        graph[4].add(new Edge(4, 3));
+    }
+
+    /**
+     * @param graph
+     * @param curr
+     * @param par
+     * @param dt[]
+     * @param low[]
+     * @param vis[]
+     * @param time
+     * @param ap[]
+     */
+    public static void dfs(
+            ArrayList<Edge> graph[],
+            int curr,
+            int par,
+            int dt[],
+            int low[],
+            boolean vis[],
+            int time,
+            boolean ap[]) {
+
+        vis[curr] = true;
+        dt[curr] = low[curr] = ++time;
+        int children = 0;
+
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            int neigh = e.dest;
+
+            if (par == neigh) {
+                continue;
+            } else if (vis[neigh]) {
+                low[curr] = Math.min(low[curr], dt[neigh]);
+            } else {
+                dfs(graph, neigh, curr, dt, low, vis, time, ap);
+                low[curr] = Math.min(low[curr], low[neigh]);
+                if (dt[curr] <= low[neigh] && par != -1) {
+                    ap[curr] = true;
+                }
+                children++;
+            }
+        }
+        if (par == -1 && children > 1) {
+            ap[curr] = true;
+        }
+    }
+
+    public static void getAP(ArrayList<Edge> graph[], int V) {
+        int dt[] = new int[V];
+        int low[] = new int[V];
+        int time = 0;
+
+        boolean vis[] = new boolean[V];
+        boolean ap[] = new boolean[V];
+
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
+                dfs(graph, i, -1, dt, low, vis, time, ap);
+            }
+        }
+        for (int i = 0; i < V; i++) {
+            if (ap[i]) {
+                System.out.println("AP : " + i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int V = 5;
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        createGraph(graph);
+        getAP(graph, V);
+    }
+}
